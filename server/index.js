7e2af34b4a74ec26e745630b57863320f29fb750
omar hisham <omar.hisham0597@outlock.com>
2022-06-17 22:08:45 +0200

    My first commit

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app=express();
+const PORT= process.env.PORT || 5000
+const authRoutes= require('./routes/auth.js');
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require('twilio')(accountSid, authToken);
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/auth',authRoutes);
+app.get('/',(req,res)=>{
+    res.send('hello, world');
+})
+app.post('/', (req, res) => {
+    const { message, user: sender, type, members } = req.body;
+
+    if(type === 'message.new') {
+        members
+            .filter((member) => member.user_id !== sender.id)
+            .forEach(({ user }) => {
+                if(!user.online) {
+                    twilioClient.messages.create({
+                        body: `You have a new message from ${message.user.fullName} - ${message.text}`,
+                        messagingServiceSid: messagingServiceSid,
+                        to: user.phoneNumber
+                    })
+                        .then(() => console.log('Message sent!'))
+                        .catch((err) => console.log(err));
+                }
+            })
+
+            return res.status(200).send('Message sent!');
+    }
+
+    return res.status(200).send('Not a new message request');
+});
+
+app.listen(PORT,()=>{
+    console.log(`server is runnig on port ${PORT}`)
+})
